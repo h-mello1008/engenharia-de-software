f6fff8aa187c35958c268a0ef6a440e6227d5218
h-mello1008 <henriquemello1008@gmail.com>
2025-05-23 13:58:30 -0300

    Update matriz_diagonal_principal.py

--- matriz_diagonal_principal.py
+++ matriz_diagonal_principal.py
@@ -1,3 +1,16 @@
+def prod_diag(matriz):
+    prod = 1
+    for i in range(len(matriz)):
+        prod *= matriz[i][i]
+    return prod
+
+def diag_princ(matriz):
+    diagonal = []
+    for i in range(min(len(matriz), len(matriz[0]))):  
+        diagonal.append(matriz[i][i])
+    return diagonal
+
+
 #Código Fonte
 linhas = int(input("Digite o número de linhas: "))
 colunas = int(input("Digite o número de colunas: "))
@@ -16,8 +29,21 @@ for i in range(linhas):
     print()
 
 #Mostrando Valores
+print("Matriz Original")
+print()
 for i in range(linhas):
     for j in range(colunas):
         print("  ", matriz[i][j], end="   ")
     print()
     print()
+
+#Produto da Diagonal
+if linhas == colunas:
+    print(f'O produto da diagonal principal é: {prod_diag(matriz)}')
+    print(diag_princ(matriz))
+
+elif linhas != colunas:
+    print("O número de linhas e colunas é diferente, portanto não é possível fazer o produto da diagonal principal")
+
+else:
+    print("Erro")
