8b327f402abbf5e973e2566cf613a57bc6bff20e
h-mello1008 <henriquemello1008@gmail.com>
2025-05-23 12:39:36 -0300

    Create matriz.py

---
+++ matriz.py
@@ -0,0 +1,23 @@
+#Código Fonte Principal
+linhas = int(input("Digite a quantidade de linhas na matriz: "))
+colunas = int(input("Digite a quantidade de colunas na matriz: "))
+print()
+
+#Construção da Matriz
+matriz = [0] * linhas
+
+for i in range(linhas):
+    matriz[i] = [0] * colunas
+
+#Recebe Valores
+for i in range(linhas):
+    for j in range(colunas):
+        matriz[i][j] = int(input(f'Digite o valor do a({i}, {j}): '))
+    print()
+
+#Mostra Valores
+for i in range(linhas):
+    for j in range(colunas):
+        print("  ", matriz[i][j], end="   ")
+    print()
+    print()
